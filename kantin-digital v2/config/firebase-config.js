ae4c2890c6acbbdc3b074c6397a55ed0d5c219e6
cynsar5e-afk <cynsar5e@gmail.com>
2026-04-15 07:41:15 +0700

    Upload 10 item(s) via Uploader Pro

---
+++ kantin-digital v2/config/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzfhmjmHxRxgbUmsBehYSkY9mV0KUnncY",
+  authDomain: "kantin-digital-a2fd4.firebaseapp.com",
+  projectId: "kantin-digital-a2fd4",
+  storageBucket: "kantin-digital-a2fd4.firebasestorage.app",
+  messagingSenderId: "807621844915",
+  appId: "1:807621844915:web:c142d94538148cbdc035c6"
+};
+
+window.FIREBASE_CONFIG = firebaseConfig;
